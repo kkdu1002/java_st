9e0e3f8d060b49e4c5c2c9fd5b515c3d1bce1cfb
kkdu1002 <63584294+kkdu1002@users.noreply.github.com>
2023-03-22 18:22:06 +0900

    Add files via upload

---
+++ While_For_az.java
@@ -0,0 +1,35 @@
+package jang3;
+
+public class While_For_az {
+
+	public static void main(String[] args) {
+		
+		for (char input = 'a'; input <= 'z';)
+		{
+			System.out.print(input);
+			input ++;
+		}
+			
+//		for (int input = 97; input < 123; input++)
+//			System.out.print((char)input);
+		
+			
+		/*
+		while(input != 'z') 
+		{
+			System.out.print(input);
+			input = (char)(input + 1);
+		}
+		*/
+		
+		/*
+		do 
+		{
+			System.out.print(input);
+			input = (char)(input + 1);
+		}
+		while (input <= 'z');
+		*/
+		
+	}
+}
