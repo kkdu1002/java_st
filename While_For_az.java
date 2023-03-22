db0b093bca9869f503db6f72f4588261ca93bca9
kkdu1002 <63584294+kkdu1002@users.noreply.github.com>
2023-03-22 18:23:12 +0900

    Update While_For_az.java

--- While_For_az.java
+++ While_For_az.java
@@ -4,10 +4,9 @@ public class While_For_az {
 
 	public static void main(String[] args) {
 		
-		for (char input = 'a'; input <= 'z';)
+		for (char input = 'a'; input <= 'z';input ++)
 		{
 			System.out.print(input);
-			input ++;
 		}
 			
 //		for (int input = 97; input < 123; input++)
