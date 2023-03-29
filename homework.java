47c1206a04ae7008820bc2da280f8c6d8b3c5593
kkdu1002 <63584294+kkdu1002@users.noreply.github.com>
2023-03-29 18:12:08 +0900

    Add files via upload

--- homework.java
+++ homework.java
@@ -1,34 +1,17 @@
-package jang7;
-
-import java.util.Scanner;
+package jang3;
 
 public class homework 
 {
-	public static void main(String[] args)
+	public static void main(String[] args) 
 	{
-		Scanner input = new Scanner(System.in);
-		
-		int people , kor , math , en , all = 0 , all_1 = 0;
-		int start;
-		System.out.print("인원수 : ");
-		people = input.nextInt();
-		
-		
-		for (start = 1; start <= people;start++) 
+
+		for(int i = 0; i < 5;i++) 
 		{
-			System.out.printf("%d) 국어점수 :",start);
-			kor = input.nextInt();
-			System.out.printf("%d) 영어점수 :",start);
-			en = input.nextInt();
-			System.out.printf("%d) 수학점수 :",start);
-			math = input.nextInt();
-			all += kor + en + math;	
-			System.out.printf("%d) 평균 : %d\n",start ,all / 3);
+			for(int j = 5;j > i; j--)
+			{
+				System.out.printf("*");
+			}
 			System.out.printf("\n");
-			all_1 += kor + en + math;		
-			all = 0;
 		}
-		System.out.printf("전체합계 : %d\n",all_1);
-		System.out.printf("전체평균 : %d\n",all_1 / 3);
 	}
 }
